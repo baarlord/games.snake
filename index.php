de302d75565e2483aa85a3902474a771105e197e
Леонид Ерёмин <eremin@intervolga.ru>
2020-06-26 23:44:33 +0400

    task-1: Push the game `snake` to GitHub.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Snake game</title>
+    <link rel="stylesheet" href="lib/css/snake-style.css">
+</head>
+<body>
+<canvas id="snake-game" width="600" height="600"></canvas>
+<div></div>
+<script src="lib/js/snake-script.js"></script>
+</body>
+</html>
